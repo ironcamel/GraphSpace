09547f4a7c4b83c8d5d16c7e674017929e184d98
Naveed Massjouni <naveedm9@gmail.com>
2011-11-03 01:49:03 -0400

    Adding an index to ddl

--- data/make-db.sql
+++ data/make-db.sql
@@ -9,10 +9,10 @@ CREATE TABLE graph (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     json TEXT,
-    graphml TEXT,
     user_id TEXT,
     FOREIGN KEY(user_id) REFERENCES user(id)
 );
+create index graph_user_id_idx on graph(user_id);
 
 CREATE TABLE graph_tag (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,122 +28,24 @@ CREATE TABLE graph_to_tag (
 );
 
 /*
-insert into user values ('test', 'test', 'test');
+insert into user values ('test', 'test', 'Test Account');
+insert into user values ('chrisp', 'chrisp', 'Chris Poirel');
+insert into user values ('chrisl', 'chrisl', 'Chris Lasher');
 insert into user values ('arjun', 'arjun', 'Arjun');
-INSERT INTO graph(id, graphml) VALUES('1','<graphml>
-  <key id="label" for="all" attr.name="label" attr.type="string"/>
-  <key id="graph_id" for="all" attr.name="graph_id" attr.type="string"/>
-  <graph>
-    <node id="1">
-      <data key="label">1</data>
-      <data key="graph_id">1</data>
-    </node>
-    <node id="2">
-      <data key="label">2</data>
-      <data key="graph_id">2</data>
-    </node>
-    <edge target="1" source="2"/>
-  </graph>
-</graphml>');
-
-INSERT INTO graph(id, graphml) VALUES('2','<graphml>
-  <key id="label" for="all" attr.name="label" attr.type="string"/>
-  <key id="graph_id" for="all" attr.name="graph_id" attr.type="string"/>
-  <graph>
-    <node id="1">
-      <data key="label">1</data>
-      <data key="graph_id">1</data>
-    </node>
-    <node id="2">
-      <data key="label">2</data>
-      <data key="graph_id">2</data>
-    </node>
-    <node id="3">
-      <data key="label">3</data>
-      <data key="graph_id">3</data>
-    </node>
-    <edge target="1" source="2"/>
-    <edge target="1" source="3"/>
-    <edge target="2" source="3"/>
-  </graph>
-</graphml>');
-
-INSERT INTO graph(id, graphml) VALUES('3','<graphml>
-  <key id="label" for="all" attr.name="label" attr.type="string"/>
-  <key id="graph_id" for="all" attr.name="graph_id" attr.type="string"/>
-  <graph>
-    <node id="1">
-      <data key="label">1</data>
-      <data key="graph_id">1</data>
-    </node>
-    <node id="2">
-      <data key="label">2</data>
-      <data key="graph_id">2</data>
-    </node>
-    <node id="3">
-      <data key="label">3</data>
-      <data key="graph_id">3</data>
-    </node>
-    <node id="4">
-      <data key="label">4</data>
-      <data key="graph_id">4</data>
-    </node>
-    <edge target="1" source="2"/>
-    <edge target="1" source="3"/>
-    <edge target="2" source="3"/>
-    <edge target="3" source="4"/>
-  </graph>
-</graphml>
-');
-
-INSERT INTO graph(id, graphml) VALUES('4','<graphml>
-  <key id="label" for="all" attr.name="label" attr.type="string"/>
-  <key id="graph_id" for="all" attr.name="graph_id" attr.type="string"/>
-  <graph>
-    <node id="1">
-      <data key="label">1</data>
-      <data key="graph_id">1</data>
-    </node>
-    <node id="2">
-      <data key="label">2</data>
-      <data key="graph_id">2</data>
-    </node>
-    <node id="3">
-      <data key="label">3</data>
-      <data key="graph_id">3</data>
-    </node>
-    <node id="4">
-      <data key="label">4</data>
-      <data key="graph_id">4</data>
-    </node>
-    <edge target="1" source="2"/>
-    <edge target="2" source="3"/>
-    <edge target="3" source="4"/>
-    <edge target="4" source="1"/>
-  </graph>
-</graphml>');
 
 INSERT INTO graph(id, json) VALUES('5', '{
     "graph": {
-       "data" : {
-          "nodes" : [
-             {
-                "id" : "1"
-             },
-             {
-                "id" : "2"
-             }
+       "data": {
+          "nodes": [
+             { "id": "1" },
+             { "id": "2" }
           ],
           "edges" : [
-             {
-                "source" : "2",
-                "target" : "1",
-                "id" : "2to1"
-             }
+             { "id": "2to1", "source": "2", "target": "1" }
           ]
        }
-    },
-    "metadata": { }
+    "metadata": { "name" : "sample 1" }
+    "metadata": { "name": "sample 1" }
 }');
 
 */
